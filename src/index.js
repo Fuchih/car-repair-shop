78ab69bff0b9309501afe3e6ab62c16c7b091022
Jin <t19887348@gmail.com>
2022-03-30 08:52:25 +0800

    preloader

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import toggleNavBar from './modules/toggleNavBar'
 import pageNavigation from './modules/pageNavigation'
 import './css/index.scss'
 
+const preloader = document.querySelector('#preloader')
+const warp = document.querySelector('#wrap')
 const barMenu = document.querySelector('.bar-container')
 const homeAnchors = document.querySelectorAll('.home-anchor')
 const aboutAnchors = document.querySelectorAll('.about-anchor')
 const serviceAnchors = document.querySelectorAll('.service-anchor')
 const contactAnchors = document.querySelectorAll('.contact-anchor')
+
+window.addEventListener('load', () => {
+  preloader.style.opacity = 0
+  preloader.style.zIndex = '-999'
+
+  setTimeout(() => {
+    wrap.style.opacity = 1
+  }, 500)
+})
 
 window.addEventListener('scroll', () => navAnimation())
 barMenu.addEventListener('click', () => toggleNavBar())
